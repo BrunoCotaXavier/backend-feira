d2b63f0c480a38e24166fea728f524712e6e6b3c
brunocota <brunocota@hotmail.com.br>
2024-10-03 18:25:14 -0300

    fix db

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.get('/listMassages', async (req, res) => {
 
 
 
-/* migration(); */
+migration();
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync(/* { force: true } */).then(() => {
     app.listen(PORT, () => {
         console.log(`### Servidor rodando na porta: ${PORT}`);
     });
