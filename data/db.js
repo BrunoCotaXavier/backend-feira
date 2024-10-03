c5ffed400246d6fd5eb16856c01f1e00a022788f
brunocota <brunocota@hotmail.com.br>
2024-10-03 18:19:11 -0300

    fix db

--- data/db.js
+++ data/db.js
@@ -6,7 +6,7 @@ const dbUser = 'root';
 const dbPassword = 'root123';
 const dbHost = '127.0.0.1';
 
-const sequelize = new Sequelize(dbName, dbUser, {
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: 'mysql',
 });
